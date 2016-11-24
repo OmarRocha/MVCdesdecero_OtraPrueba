530fe609aa35f49055afb317ef85c306adf813e8
Omar Rocha <onrg@msw.com.mx>
2016-11-24 16:11:29 -0600

    Agregar archivos de proyecto.

---
+++ MVCdesdecero_OtraPrueba/Models/ProductView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCdesdecero_OtraPrueba.Models
+{
+    public class ProductView
+    {
+        [Key] //<-- el tributo ID va ser el codigo del producto o clave autonumerica
+        public int ProductID { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime LastBuy { get; set; }
+
+        public float Stock { get; set; }
+    }
+}
